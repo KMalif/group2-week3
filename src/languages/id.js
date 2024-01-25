30e6d4421fd21d712b3040e78990aac0f33d819f
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2024-01-25 17:03:06 +0700

    add: translating

--- src/languages/id.js
+++ src/languages/id.js
@@ -7,4 +7,11 @@ export default {
   app_popup_error_title: 'Terjadi kesalahan!',
   app_popup_error_message: 'Maaf, telah terjadi kesalahan. Silakan coba beberapa saat lagi',
   app_popup_close_button_label: 'Tutup',
+  app_header_title: 'Semua yang perlu Anda ketahui tentang Artikel kami',
+  app_header_sub_title:
+    'Lorem Ipsum hanyalah teks tiruan dari industri percetakan dan penyusunan huruf. Lorem Ipsum telah menjadi teks tiruan standar industri sejak tahun 1500-an, ketika seorang pencetak yang tidak dikenal mengambil kumpulan teks dan mengacaknya untuk membuat buku contoh huruf. Ia tidak hanya bertahan selama lima abad, tetapi juga lompatan ke dalam penyusunan huruf elektronik, namun tetap tidak berubah. Ini dipopulerkan pada tahun 1960an dengan dirilisnya lembaran Letraset yang berisi bagian-bagian Lorem Ipsum, dan baru-baru ini dengan perangkat lunak desktop publishing seperti Aldus PageMaker yang menyertakan versi Lorem Ipsum.',
+  app_header_button: 'Buat Artikel',
+  app_main_content_text: 'Artikel',
+  app_search_button: 'Cari',
+  app_main_content_description_article: 'Apakah konser rock benar-benar kembali populer?',
 };
